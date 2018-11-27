723cebc8d73ea1601e3da3a86ca23a4576a5b484
Louis Wilbrink <louis.wilbrink@gmail.com>
2018-11-27 15:10:43 -0800

    Add pins to map.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,30 @@ import { Component, OnInit } from '@angular/core';
 
 export class AppComponent implements OnInit {
   title = 'oven-fresh';
+  mapResults = {
+    type: 'FeatureCollection',
+    features: [{
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-77.032, 38.913]
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Washington, D.C.'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [-122.414, 37.776]
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'San Francisco, California'
+      }
+    }]
+  };
 
   ngOnInit() {
   }
--- src/app/app/map/map.component.ts
+++ src/app/app/map/map.component.ts
@@ -1,23 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, OnChanges {
+
+  map: {};
+
+  @Input('mapResults') results;
 
   constructor() { }
 
   ngOnInit() {
     // Mapbox configuration.
     mapboxgl.accessToken = 'pk.eyJ1IjoibG91aXN3aWxicmluayIsImEiOiJjajB1MnR1ZzcwNXE0MndvNnM3amszOGEyIn0.fq259e8DyvBrFYoumz_9eQ';
-      var map = new mapboxgl.Map({
+
+    this.map = new mapboxgl.Map({
       container: 'map',
+      center: [-122.420679, 37.772537],
+      zoom: 13,
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+
+    // Add markers to map.
+    this.results.features.forEach(function(marker) {
+      // create a HTML element for each feature
+      var el = document.createElement('div');
+      el.className = 'marker';
+
+      // Make a marker for each feature and add it to the map.
+      new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(this.map);
+    }, this);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
   }
 }
