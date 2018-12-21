cac4d4c4b429817080a85ac1641cd652b31c3c0b
Louis Wilbrink <louis.wilbrink@gmail.com>
2018-12-21 13:24:21 -0800

    Add results and place component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './app/map/map.component';
 import { SearchComponent } from './search/search.component';
+import { ResultsComponent } from './results/results.component';
+import { PlaceComponent } from './place/place.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    SearchComponent
+    SearchComponent,
+    ResultsComponent,
+    PlaceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/place/place.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-place',
+  templateUrl: './place.component.html',
+  styleUrls: ['./place.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class PlaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
